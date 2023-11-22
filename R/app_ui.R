74c48ffef9537dde4ed4d0d314e678b743bbb90e
TomChupan <tomas.chupan@seznam.cz>
2023-11-22 12:46:07 +0100

    Updated title of the app

--- R/app_ui.R
+++ R/app_ui.R
@@ -11,7 +11,7 @@ app_ui <- function(request) {
     golem_add_external_resources(),
     # Your application UI logic
     dashboardPage(
-      dashboardHeader(title = h1("proteoME app")
+      dashboardHeader(title = "proteoME app"
                       ),
       dashboardSidebar(width = '22%'
                        ),
