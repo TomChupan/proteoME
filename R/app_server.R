2af60d7067bb4be9a4813479c8b534181673509f
TomChupan <tomas.chupan@seznam.cz>
2023-12-22 17:35:29 +0100

    Du petit r approach

--- R/app_server.R
+++ R/app_server.R
@@ -7,6 +7,13 @@
 app_server <- function(input, output, session) {
   # Your application server logic
   #Max File Size:
-  n=100 #file size limit (MB)
-  options(shiny.maxRequestSize=n*1024^2)
+  options(shiny.maxRequestSize=100*1024^2) #100 = file size limit (MB)
+  #when changing this limit - don't forgot to change a placeholder in a fileInput
+  #within a mod_upload
+
+  r=reactiveValues(
+    d1=c()
+  )
+  mod_sidebar_server("sidebar_1",r=r)
+  mod_body_server("body_1",r=r)
 }
