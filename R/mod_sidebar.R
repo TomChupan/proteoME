db459e7b2feb58558fd8ea29c3afb32f70c2f344
TomChupan <tomas.chupan@seznam.cz>
2023-12-22 17:36:38 +0100

    Conditional panels, replication of modules...

--- R/mod_sidebar.R
+++ R/mod_sidebar.R
@@ -13,24 +13,43 @@ mod_sidebar_ui <- function(id){
   ns <- NS(id)
   tagList(
 
-  )
+    conditionalPanel(condition = "input.tabs=='im'",
+    mod_upload_ui(ns("upload_1"),
+                  menuItem_label="Abundances",
+                  fileInput_label="Upload your dataset with abundances (.csv/.txt)"),
+    mod_upload_ui(ns("upload_2"),
+                  menuItem_label="Annotations (run)",
+                  fileInput_label="Upload your run annotation dataset (.csv/.txt)"),
+    mod_upload_ui(ns("upload_3"),
+                  menuItem_label="Annotations (sample)",
+                  fileInput_label="Upload your sample annotation dataset (.csv/.txt)")
+
+    ) #conditionalPanel close
+
+  ) #tagList close
 }
 
 #' sidebar Server Functions
 #'
+#' @param r A "storage" for the variables used throughout the app
+#'
 #' @rdname mod_sidebar
 #' @export
-mod_sidebar_server <- function(id){
+mod_sidebar_server <- function(id,r){
   moduleServer(
     id,
     function(input, output, session){
     ns <- session$ns
 
+    mod_upload_server("upload_1",data_type=1,r=r)
+    mod_upload_server("upload_2",data_type=2,r=r)
+    mod_upload_server("upload_3",data_type=3,r=r)
+
   })
 }
 
 ## To be copied in the UI
-# mod_sidebar_ui("sidebar_1")
+#
 
 ## To be copied in the server
-# mod_sidebar_server("sidebar_1")
+#
