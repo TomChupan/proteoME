4de9b6e8240f26956336ec0fd98d300f97f3136d
TomChupan <tomas.chupan@seznam.cz>
2024-01-20 11:50:38 +0100

    Added new module and packages

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -25,6 +25,7 @@ golem::add_module(name = "upload", with_test = TRUE)
 golem::add_module(name = "sidebar", with_test = TRUE)
 golem::add_module(name = "body", with_test = TRUE)
 golem::add_module(name = "body_tab1", with_test = TRUE)
+golem::add_module(name = "factors", with_test = TRUE)
 
 ## Add helper functions ----
 ## Creates fct_* and utils_*
@@ -51,6 +52,8 @@ usethis::use_package("vroom")
 usethis::use_package("Hmisc")
 usethis::use_package("DT")
 usethis::use_package("shinyjs")
+usethis::use_package("magrittr")
+usethis::use_package("dplyr")
 ## Check if you haven't missed anything:
 # This function will read all the scripts in the R/ folder and
 # try to guess required dependencies
