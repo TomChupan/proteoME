09e512e5eb2ab3c863786564f7db70fd668cff89
TomChupan <tomas.chupan@seznam.cz>
2024-03-18 20:36:20 +0100

    Function with individual summary statistics
    It is used in the mod_body_sumtab module.

---
+++ R/fct_Tsummary.R
@@ -0,0 +1,17 @@
+#' Tsummary
+#'
+#' @description An individual summary function to be used in a 'Transformation' tab
+#' in the proteoME app. The input is a vector (or a column from a data frame).
+#'
+#' @return A data frame with selected summary statistics.
+#'
+#' @noRd
+Tsummary=function(x){
+  s=data.frame(min=min(x,na.rm = T),q25=quantile(x,0.25,na.rm = T),
+               median=median(x,na.rm = T),mean=mean(x,na.rm = T),
+               q75=quantile(x,0.75,na.rm = T),max=max(x,na.rm = T),
+               range_diff=range(x,na.rm = T)[2]-range(x,na.rm = T)[1],
+               skewness=DescTools::Skew(x,na.rm = T),row.names = "")
+  colnames(s)[colnames(s)=="range_diff"]="max-min"
+  s
+}
