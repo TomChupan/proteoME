20c27fc5613ab7282d17adbf213b154a7c60271e
TomChupan <tomas.chupan@seznam.cz>
2024-03-18 20:31:16 +0100

    Dataset documentation
    Modification in response to the new datasets.

--- R/data.R
+++ R/data.R
@@ -3,15 +3,15 @@
 #' @description This dataset contains sample data with protein abundances to be used in a
 #' proteoME Shiny app.
 #'
-#' @format ## data_example
-#' A data frame with 18 rows and 7 columns:
+#' @format
+#' A data frame with 2018 rows and 101 columns:
 #' \describe{
 #' \itemize{
-#'  \item{Accession}{An UniProt accession number – a unique identifier assigned
+#'  \item{Accession: }{An UniProt accession number – a unique identifier assigned
 #'  to a protein entry in the UniProt Knowledgebase (UniProtKB).
 #'  See \url{https://www.uniprot.org/} }
-#'  \item{Abundances_F#}{Columns with protein abundances for each run
-#'  (several runs may come from one patient – repetitions)}
+#'  \item{F#: }{Columns with protein abundances for each run
+#'  (several runs may come from one patient – replicates)}
 #'  \itemize{
 #'  \item Abundances can be raw, imputed or normalized (or both imputed and
 #'   normalized)
@@ -19,7 +19,7 @@
 #'  }
 #'
 #' }
-#' @source <https://raw.githubusercontent.com/TomChupan/data/main/data_example.csv>
+#' @source <https://raw.githubusercontent.com/TomChupan/data/main/data_example3.csv>
 #'
 "data_example"
 
@@ -28,8 +28,8 @@
 #' @description This dataset contains run annotations for *data_example* dataset
 #' to be used in a proteoME Shiny app.
 #'
-#' @format ## ann_run_example
-#' A data frame with 6 rows and 3 columns:
+#' @format
+#' A data frame with 100 rows and 6 columns:
 #' \describe{
 #'  \item{runID}{A column with run IDs in a form of F# (where # is a number). Each ID
 #'  has to be unique.}
@@ -37,11 +37,14 @@
 #'  a letter indicating a treatment group and a number indicating the patient
 #'  number within the group. E.g. ID A2 stands for the second patient within a
 #'  treatment group A. The same sample ID can be assigned to several run IDs.}
-#'  \item{rep}{A column with repetition number. E.g. Run F4 is the second
-#'  repetition for a patient A2.}
+#'  \item{rep}{A column with replicate number. E.g. Run F4 is the second
+#'  replicate for a patient A2.}
+#'  \item{batchNo}{Batch number.}
+#'  \item{batchOrder}{Order of the run within the batch.}
+#'  \item{dateTime}{Date and time of the measurement in a \%m/\%d/\%Y \%R format.}
 #'
 #' }
-#' @source <https://raw.githubusercontent.com/TomChupan/data/main/data_example_run.csv>
+#' @source <https://raw.githubusercontent.com/TomChupan/data/main/data_example_run3.csv>
 #'
 "ann_run_example"
 
@@ -50,21 +53,20 @@
 #' @description This dataset contains sample annotations for *data_example* dataset
 #' to be used in a proteoME Shiny app.
 #'
-#' @format ## ann_sample_example
-#' A data frame with 3 rows and 5 columns:
+#' @format
+#' A data frame with 50 rows and 5 columns:
 #' \describe{
-#'  \item{sampleID}{A column with **unique** sample IDs (one for each patient). It consists of
+#'  \item{sampleID: }{A column with **unique** sample IDs (one for each patient). It consists of
 #'  a letter indicating a treatment group and a number indicating the patient
 #'  number within the group. E.g. ID A2 stands for the second patient within a
 #'  treatment group A.}
-#'  \item{treatment}{A column with a treatment group specification (default: only
-#'  a letter).}
-#'  \item{sex}{Male (M) or female (F).}
-#'  \item{age}{Age in years.}
-#'  \item{weight_kg}{Weight in kg.}
+#'  \item{treatment: }{A column with a treatment group specification.}
+#'  \item{sex: }{Male (M) or female (F).}
+#'  \item{age: }{Age in years.}
+#'  \item{weight_kg: }{Weight in kg.}
 #'
 #'
 #' }
-#' @source <https://raw.githubusercontent.com/TomChupan/data/main/data_example_sample.csv>
+#' @source <https://raw.githubusercontent.com/TomChupan/data/main/data_example_sample2.csv>
 #'
 "ann_sample_example"
